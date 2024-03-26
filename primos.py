1ddf2e0c0bec24c034586388525e4c5842101e5b
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-03-26 13:24:02 -0300

    Commit números primos

---
+++ primos.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+def is_primo(value):
+    div = 2
+    while div < value:
+        if value % div == 0:
+            return False
+        div += 1
+    return True
+
+class TestPrimos(unittest.TestCase):
+    def test_1(self):
+        result = is_primo(1)
+        self.assertEqual(result, True)
+
+    def test_2(self):
+        result = is_primo(2)
+        self.assertEqual(result, True)
+   
+    def test_3(self):
+        result = is_primo(3)
+        self.assertEqual(result, True)
+    
+    def test_4(self):
+        result = is_primo(4)
+        self.assertEqual(result, False)
+
+    def test_5(self):
+        result = is_primo(5)
+        self.assertEqual(result, True)
+
+    def test_6(self):
+        result = is_primo(6)
+        self.assertEqual(result, False)
+
+    def test_9(self):
+        result = is_primo(9)
+        self.assertEqual(result, False)
+
+    def test_17(self):
+        result = is_primo(17)
+        self.assertEqual(result, True)
+
+    def test_73(self):
+        result = is_primo(73)
+        self.assertEqual(result, True)
+    
+    def test_125(self):
+        result = is_primo(125)
+        self.assertEqual(result, False)
+
+
+unittest.main()
